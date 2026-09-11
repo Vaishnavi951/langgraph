e44a3e61c5bc7458fe86db0ec69a6efc60e379a3
Sasanoor Vaishnavi <vaishnavisasanoor@gmail.com>
2026-09-11 22:13:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -312,6 +312,5 @@ if __name__ == "__main__":
         app,
         host="0.0.0.0",
         port=port
-    )= "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    ) 
     uvicorn.run(app, host="0.0.0.0", port=port)
